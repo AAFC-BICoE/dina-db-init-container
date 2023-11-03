ee418c0c458c11c48bf48a650ad2bf0d94db68fd
Christian Gendreau <christiangendreau@gmail.com>
2023-11-03 15:11:04 -0400

    32213 Make dina-db-init-container more generic added generic database and user scripts. allow to use v2 from init.sh

---
+++ scripts/createUser.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script will only run if the database does NOT already exist
+# arguments: (1) database name, (2) user name, (3) user password
+
+set +e
+
+export PGPASSWORD="$POSTGRES_PASSWORD"
+
+user_exists=$(psql -U "$POSTGRES_USER" -h "$POSTGRES_HOST" "$POSTGRES_DB" -qt -c "SELECT 1 FROM pg_roles WHERE rolname = '$2'")
+
+if [ "$user_exists" = "1" ]; then
+  echo "User $1 already exists. Skipping..."
+else
+  echo "Creating user $1"
+  psql -U "$POSTGRES_USER" -h "$POSTGRES_HOST" "$POSTGRES_DB" -qt -c "CREATE USER $2 NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT"
+
+  psql -q -U "$POSTGRES_USER" -h "$POSTGRES_HOST" "$POSTGRES_DB" -qt -c "ALTER USER $2 WITH PASSWORD '$3'"
+
+  echo "Grant connect to user $2 on database $1"
+  psql -U "$POSTGRES_USER" -h "$POSTGRES_HOST" "$POSTGRES_DB" -qt -c "GRANT CONNECT ON DATABASE $1 TO $2;"
+fi
--- scripts/init.sh
+++ scripts/init.sh
@@ -5,31 +5,36 @@
 export PGPASSWORD="$POSTGRES_PASSWORD"
 export MIGRATION_USER=migration_user
 export WEB_USER=web_user
-db_array=($DINA_DB)
-for curr_db in ${db_array[@]}; do
-  echo "DINA database : ${curr_db}"
-  mu_var=MIGRATION_USER_${curr_db}
-  mu_pwd_var=MIGRATION_USER_PW_${curr_db}
-  wu_var=WEB_USER_${curr_db}
-  wu_pwd_var=WEB_USER_PW_${curr_db}
-  db_schema_name=${curr_db}
-  pg_ext_var=PG_EXTENSION_${curr_db}
-  pg_ext=${!pg_ext_var}
 
-  db_prefix_var=PREFIX_${curr_db}
-  db_prefix=${!db_prefix_var}
+if [ -n "$USE_V2" ]; then
+  echo "Using script v2"
+  ./createDatabase.sh "${DB_NAME}"
+  ./createUser.sh "${DB_NAME}" "${DB_USER}" "${DB_PASSWORD}"
+else
+  db_array=($DINA_DB)
+  for curr_db in ${db_array[@]}; do
+    echo "DINA database : ${curr_db}"
+    mu_var=MIGRATION_USER_${curr_db}
+    mu_pwd_var=MIGRATION_USER_PW_${curr_db}
+    wu_var=WEB_USER_${curr_db}
+    wu_pwd_var=WEB_USER_PW_${curr_db}
+    db_schema_name=${curr_db}
+    pg_ext_var=PG_EXTENSION_${curr_db}
+    pg_ext=${!pg_ext_var}
 
-  if [ -n "$db_prefix" ]; then
-    echo "Database prefix provided : ${db_prefix}"
-    curr_db=${db_prefix}_${curr_db}
-  fi
+    db_prefix_var=PREFIX_${curr_db}
+    db_prefix=${!db_prefix_var}
 
-  ./createDinaDatabase.sh ${curr_db} ${db_schema_name} ${!mu_var} ${!mu_pwd_var} ${!wu_var} ${!wu_pwd_var}
+    if [ -n "$db_prefix" ]; then
+      echo "Database prefix provided : ${db_prefix}"
+      curr_db=${db_prefix}_${curr_db}
+    fi
 
-  if [ -n "$pg_ext" ]; then
-    echo "Postgres Extension : ${pg_ext}"
-    ./createPostgreExtension.sh ${pg_ext} ${curr_db} ${db_schema_name}
-  fi
+    ./createDinaDatabase.sh ${curr_db} ${db_schema_name} ${!mu_var} ${!mu_pwd_var} ${!wu_var} ${!wu_pwd_var}
 
-done
-
+    if [ -n "$pg_ext" ]; then
+      echo "Postgres Extension : ${pg_ext}"
+      ./createPostgreExtension.sh ${pg_ext} ${curr_db} ${db_schema_name}
+    fi
+  done
+fi
